ecc4f2f66be04a4532520d0b56fe65e9b247cf09
Nikolai <nvanbaak@gmail.com>
2021-01-25 11:47:12 -0800

    removed contact page, edited summary section

--- portfolio.js
+++ portfolio.js
@@ -1,29 +1,34 @@
 const portfolioEntries = [
     {
+        id:0,
         title: "CityScoop",
         imgURL: "./Assets/logo-black-accent.png",
         github: "CityScoop",
         desc: "A one-stop shop for all the information you need when moving to a new city."
     },
     {
+        id:1,
         title: "Legends of Javascript",
         imgURL: "./Assets/legends-of-javascript-thumbnail.png",
         github: "legends-of-javascript",
         desc: "Want to test your javascript skills against the inevitable robot takeover?  Try this quiz game!"
     },
     {
+        id:2,
         title: "Cryptoforge",
         imgURL: "./Assets/cryptoforge-thumbnail.png",
         github: "cryptoforge",
         desc: "It's a really metal-looking password generator, not a bitcoin miner.  Sorry to get your hopes up."
     },
     {
+        id:3,
         title: "Command Center",
         imgURL: "./Assets/command-center-thumbnail.png",
         github: "command-center",
         desc: "A customizable homepage replacement site to give easy access to your favorite sites."
     },
     {
+        id:4,
         title: "Pocket Butler",
         imgURL: "./Assets/pocket-butler-thumbnail.png",
         github: "pocket-butler",
